4ba369f1be1550b37e4eb79c5ff1662cc1b855e3
Vandan Chauhan <vandan.chauhan@plughr.com>
2019-03-05 06:22:45 +0530

    Updates

---
+++ precache-manifest.06b91fc837b3ba6b03c9c41c4aeeaf7a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "40b678519cafdfe5b217fff19f2bb3d8",
+    "url": "/UpGradassignment/static/media/user.40b67851.png"
+  },
+  {
+    "revision": "e29098857728c68769c2dc85670c7b58",
+    "url": "/UpGradassignment/static/media/empt.e2909885.png"
+  },
+  {
+    "revision": "4eb091aa3ad167231a31",
+    "url": "/UpGradassignment/static/js/runtime~main.4eb091aa.js"
+  },
+  {
+    "revision": "317b0565d7c389b7f697",
+    "url": "/UpGradassignment/static/js/main.317b0565.chunk.js"
+  },
+  {
+    "revision": "e2a032349bc2797c4db5",
+    "url": "/UpGradassignment/static/js/2.e2a03234.chunk.js"
+  },
+  {
+    "revision": "317b0565d7c389b7f697",
+    "url": "/UpGradassignment/static/css/main.96b07539.chunk.css"
+  },
+  {
+    "revision": "0d7234b3b3b3167ac7eb61945293ee9b",
+    "url": "/UpGradassignment/index.html"
+  }
+];
